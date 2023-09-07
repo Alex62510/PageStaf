60cdca88ccdb6395c0646f52753dcd632fed6a17
Alex62510 <eagle625@tut.by>
2023-09-07 16:04:13 +0300

    4 created Redux App with PTK

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,6 +1,7 @@
 const {prisma} = require('../prisma/prisma-client')
 
 
+
 const all = async (req, res) => {
     try {
         const employees = await prisma.employee.findMany()
@@ -27,7 +28,53 @@ const add = async (req, res) => {
         res.status(500).json({message: 'Something wrong'})
     }
 }
+
+const remove = async (req, res) => {
+    const {id} = req.body
+    try {
+        await prisma.employee.delete({
+            where: {
+                id
+            }
+        })
+        res.status(204).json('ok')
+    } catch {
+        res.status(500).json({message: "Can't remove employee"})
+    }
+}
+const edit = async (req, res) => {
+    const data = req.body
+    const id = data.id
+    try {
+        await prisma.employee.update({
+            where: {
+                id,
+            },
+            data
+        })
+        res.status(204).json('ok')
+    } catch {
+        res.status(500).json({message: "Can't edit employee"})
+    }
+}
+
+const employee = async (req, res) => {
+    const {id} = req.params
+    try {
+        const employee = await prisma.employee.findUnique({
+            where: {
+                id
+            }
+        })
+        res.status(200).json(employee)
+    } catch {
+        res.status(500).json({message: "Can't get employee"})
+    }
+}
 module.exports = {
     all,
-    add
+    add,
+    remove,
+    edit,
+    employee
 }
--- routes/employees.js
+++ routes/employees.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const {auth} = require('../middleware/auth')
-const {all,add} = require("../controllers/employees");
+const {all, add, remove, employee, edit} = require("../controllers/employees");
 
 // const router = Router()
 
 router.get('/', auth, all)
-router.get('/:id', auth, () => console.log('get single employee'))
+router.get('/:id', auth, employee)
 router.post('/add', auth, add)
-router.post('/remove/:id', auth, () => console.log('remove employee'))
-router.put('/edit/:id', auth, () => console.log('edit employee'))
+router.post('/remove/:id', auth, remove)
+router.put('/edit/:id', auth, edit)
 module.exports = router
