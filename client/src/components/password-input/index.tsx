29d8034c1707a7c49506bee3a79ec7798aa5a203
Alex62510 <eagle625@tut.by>
2023-09-10 21:41:24 +0300

    10 custom-input-password

---
+++ client/src/components/password-input/index.tsx
@@ -0,0 +1,40 @@
+import {Form, Input} from 'antd'
+import {NamePath} from 'antd/es/form/interface'
+
+type Props = {
+    name: string;
+    placeholder: string;
+    dependencies?: NamePath[]
+}
+export const PasswordInput = ({name, placeholder, dependencies}: Props) => {
+    return (
+        <Form.Item
+            name={name}
+            dependencies={dependencies}
+            hasFeedback
+            rules={[{
+                required: true,
+                message: 'required field'
+            }, ({getFieldValue}) => ({
+                validator(_, value) {
+                    if (!value) {
+                        return Promise.resolve()
+                    }
+                    if (name === 'confirmPassword') {
+                        if (!value || getFieldValue(('password')) === value) {
+                            return Promise.resolve()
+                        }
+                        return Promise.reject(new Error('Password must be the same'))
+                    } else {
+                        if (value.level < 6) {
+                            return Promise.reject(new Error('Password must be longer then 6 symbols'))
+                        }
+                        return Promise.resolve()
+                    }
+                }
+            })]}
+        >
+            <Input.Password placeholder={placeholder} size='large'></Input.Password>
+        </Form.Item>
+    )
+}
--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,14 +1,29 @@
 import {Layout} from "../../components/layout";
-import {Card, Form, Row} from "antd";
+import {Card, Form, Row, Space, Typography} from "antd";
+import {CustomInput} from "../../components/custom-input";
+import {PasswordInput} from "../../components/password-input";
+import {CustomButton} from "../../components/custom-batton";
+import {Link} from "react-router-dom";
+import {Paths} from "../../paths";
 
 export const Login = () => {
     return (
         <Layout>
             <Row align="middle" justify='center'>
                 <Card title='Enter' style={{width: '30rem'}}>
-                    <Form onFinish={()=>{}}>
-
+                    <Form onFinish={() => {
+                    }}>
+                        <CustomInput type='email' name='email' placeholder='Email'/>
+                        <PasswordInput name='password' placeholder='Password'/>
+                        <CustomButton type='primary' htmlType='submit'>
+                            Enter
+                        </CustomButton>
                     </Form>
+                    <Space direction='vertical' size='large'>
+                        <Typography.Text>
+                            No account <Link to={Paths.register}>Register</Link>
+                        </Typography.Text>
+                    </Space>
                 </Card>
             </Row>
         </Layout>
