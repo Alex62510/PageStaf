bb4fdfaccfbdfdcb21d63123f9bd61cc55835f39
Alex62510 <eagle625@tut.by>
2023-09-11 16:10:16 +0300

    13 utils folder

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -5,14 +5,30 @@ import {PasswordInput} from "../../components/password-input";
 import {CustomButton} from "../../components/custom-batton";
 import {Link} from "react-router-dom";
 import {Paths} from "../../paths";
+import {useLoginMutation, UserData} from "../../app/services/auth";
+import {isErrorMessage} from "../../utils/is-error-message";
+import {useState} from "react";
 
 export const Login = () => {
+    const [loginUser, loginUserResult] = useLoginMutation()
+    const [error, setError] = useState('')
+    const login = async (data: UserData) => {
+        try {
+            await loginUser(data).unwrap()
+        } catch (err) {
+            const maybeError = isErrorMessage(err)
+            if (maybeError) {
+                setError(err.data.message)
+            } else {
+                setError("Unknown error")
+            }
+        }
+    }
     return (
         <Layout>
             <Row align="middle" justify='center'>
                 <Card title='Enter' style={{width: '30rem'}}>
-                    <Form onFinish={() => {
-                    }}>
+                    <Form onFinish={login}>
                         <CustomInput type='email' name='email' placeholder='Email'/>
                         <PasswordInput name='password' placeholder='Password'/>
                         <CustomButton type='primary' htmlType='submit'>
