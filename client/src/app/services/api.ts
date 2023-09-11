01a07db115098daf35f4344fc8611a19f98624b0
Alex62510 <eagle625@tut.by>
2023-09-11 13:56:38 +0300

    12 api auth

--- client/src/app/services/api.ts
+++ client/src/app/services/api.ts
@@ -1,8 +1,13 @@
-import {createApi,fetchBaseQuery, retry} from '@reduxjs/toolkit/query/react'
-import {RootState} from "../store";
-const baseQuery=fetchBaseQuery({
-    baseUrl:'http://localhost:8000/api',
-    prepareHeaders:(headers,{getState})=>{
-const token=(getState() as RootState).auth
-    }
+import {createApi, fetchBaseQuery, retry} from '@reduxjs/toolkit/query/react'
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: 'http://localhost:8000/api',
+
+})
+const baseQueryWithRetry = retry(baseQuery, {maxRetries: 1})
+export const api = createApi({
+    reducerPath: 'splitApi',
+    baseQuery: baseQueryWithRetry,
+    refetchOnMountOrArgChange: true,
+    endpoints: () => ({})
 })
